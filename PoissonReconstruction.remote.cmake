cf55528299c3dcbf919fae46058af6da709570f1
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-09 11:51:15 -0400

    Update PoissonReconstruction.remote.cmake

--- PoissonReconstruction.remote.cmake
+++ PoissonReconstruction.remote.cmake
@@ -5,6 +5,6 @@
 itk_fetch_module(PoissonReconstruction
   "Poisson Surface reconstruction from unorganized points"
   GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
-  # July 5, 2015 - first working as a remote module
-  GIT_TAG 79a477b0dcff4a0d985845d517c4ae065b9892ba
+  # September 9, refactored remote module
+  GIT_TAG 137d751c218ffbd946e9b9de6c108666af54c2d3
   )
