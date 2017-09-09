f3af082c2093fa3e40922daf4bbe16e5f53b0201
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-09 13:55:46 -0400

    ENH: Bump version

--- PoissonReconstruction.remote.cmake
+++ PoissonReconstruction.remote.cmake
@@ -5,6 +5,6 @@
 itk_fetch_module(PoissonReconstruction
   "Poisson Surface reconstruction from unorganized points"
   GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
-  # September 9, refactored remote module
-  GIT_TAG 137d751c218ffbd946e9b9de6c108666af54c2d3
+  # September 10, refactored remote module
+  GIT_TAG b858cb7c0afa4d48cda109c0ad188644a1162018
   )
