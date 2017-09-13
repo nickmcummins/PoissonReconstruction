ffaa97c271b8dfb91d7742dd3c05d7377d864afe
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-13 19:22:06 -0400

    BUG: itk_fetch_module to vtk_fetch_module

--- PoissonReconstruction.remote.cmake
+++ PoissonReconstruction.remote.cmake
@@ -2,9 +2,9 @@
 # Poisson Reconstruction
 #
 
-itk_fetch_module(PoissonReconstruction
+vtk_fetch_module(PoissonReconstruction
   "Poisson Surface reconstruction from unorganized points"
   GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
-  # September 10, refactored remote module
-  GIT_TAG b858cb7c0afa4d48cda109c0ad188644a1162018
+  # September 10, 2017 refactored remote module
+  GIT_TAG f3af082c2093fa3e40922daf4bbe16e5f53b0201
   )
